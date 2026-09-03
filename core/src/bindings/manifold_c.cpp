1edebd09bed17b5f462476a11d0f7c899e9980fd
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-09-03 16:30:34 +0300

    updat

--- core/src/bindings/manifold_c.cpp
+++ core/src/bindings/manifold_c.cpp
@@ -1,5 +1,9 @@
 #include <manifold/manifoldc.h>
 
+// Reason why we have this method here
+// The Zig compiler incorrectly calculates register exhaustion when calling C functions with a large number of mixed arguments (specifically floating-point values and structs). Instead of utilizing remaining available hardware registers, Zig prematurely spills the arguments onto the stack. When the C++ function pops them off expecting the strict x86_64 System V ABI layout, the memory offsets are misaligned, causing the values to scramble or merge with adjacent variables
+// More info: https://github.com/ziglang/zig/issues/22515
+
 extern "C" {
     // Passes the 12-element matrix as a single pointer to bypass ABI register exhaustion
     ManifoldManifold* manifold_transform_array(void* mem, ManifoldManifold* m, const double* mat) {
